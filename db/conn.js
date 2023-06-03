01c87174c7d7088bcff42d3044b470822e81554f
himangshumahato82 <himangshumahato82@gmail.com>
2023-06-03 09:50:25 +0530

    done

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const DB = process.env.DATABASE
-
+mongoose.set('strictQuery', true);
 mongoose.connect(DB,{
     useUnifiedTopology:true,
     useNewUrlParser:true
